b78c9c3f8bb3f1380b747e5268a4ba8c8bdeee61
Tanay Kamath <2tanaykamath@gmail.com>
2020-08-01 01:51:16 +0530

    Unversioned files comitted

---
+++ app/src/main/java/com/myapp/beatify/OtherSongsAdapter.java
@@ -0,0 +1,52 @@
+package com.myapp.beatify;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class OtherSongsAdapter extends RecyclerView.Adapter<OtherSongsAdapter.MyViewHolder> {
+    private List<CreateSong> mSongList;
+
+    public OtherSongsAdapter(List<CreateSong> mSongList) {
+        this.mSongList = mSongList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        view= LayoutInflater.from(parent.getContext()).inflate(R.layout.create_other_songs,parent,false);
+        MyViewHolder myViewHolder=new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.imageView.setImageResource(mSongList.get(position).getImg());
+        holder.textView.setText(mSongList.get(position).getTxt());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSongList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        public ImageView imageView;
+        public TextView textView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imageView = itemView.findViewById(R.id.recent_img);
+            this.textView = itemView.findViewById(R.id.recent_txt);
+        }
+    }
+}
